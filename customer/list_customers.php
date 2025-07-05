b0302ed5f471d4e8575c6fed152c2793c6225fbd
Kalmi Dilara <143012202+Kalmid@users.noreply.github.com>
2025-07-05 18:25:49 +0530

    Update list_customers.php

--- customer/list_customers.php
+++ customer/list_customers.php
@@ -23,9 +23,22 @@ $result = $stmt->get_result();
 <head>
     <title>All Customers</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> 
 </head>
 <body class="container mt-5">
-    <h2>Customer List</h2>
+
+<header style="display: flex; justify-content: space-between; padding: 10px 20px; background-color: #f9fafa; border-bottom: 1px solid #ddd;">
+  <nav>
+    <a href="../customer/list_customers.php" style="margin-right: 15px; text-decoration: none; color: #555;">Customers</a>
+    <a href="../item/list_items.php" style="text-decoration: none; color: #555;">Items</a>
+<a href="../reports/item_report.php" style="margin-right: 15px; text-decoration: none; color: #555;">Item Report</a>
+<a href="../reports/invoice_item_report.php" style="margin-right: 15px; text-decoration: none; color: #555;">Invoice Item Report</a>
+<a href="../reports/invoice_report.php" style="margin-right: 15px; text-decoration: none; color: #555;">Invoice Report</a>
+  </nav>
+</header>
+
+	<br>
+    <h2 class="mb-4 text-center">Customer List</h2>
     <a href="add_customer.php" class="btn btn-success mb-3">+ Add New Customer</a>
 
 <form method="GET" class="mb-3">
